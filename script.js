6cf005e0f1e1c9c6ef4fcf2b76948216e0e05ce7
martinbugayong <martinbugayong@gmail.com>
2019-06-27 21:01:54 -0700

    refactor code

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const username = document.getElementById('userName');
+const password = document.getElementById('password');
+
 const users = [
     {
         name: 'gerrymi',
@@ -9,48 +12,23 @@ const users = [
     }
 ]
 
-// function clickLogin (){
-//     let username = document.getElementById('userName').value;
-//     let password = document.getElementById('password').value;
-//     let currentUser = users.find(function(user){
-//         return user.name === username;
-//     })
-//     /* the higherorder function ".find" a method in JS. it is the same as the forloop below
-
-//     for (i = 0; i < users.length; i++) {
-//         if (username === users[i].name) {
-//             currentUser = users[i]
-//         }
-//     }
-//     */
-//     if (username === currentUser.name &&
-//         password === currentUser.password
-//     ){
-//         window.location = 'yeup.html'
-//     } else {
-//         window.location = 'nope.html'
-//     }   
-// }
-
 function enterKeyLogin (event){
-    let username = document.getElementById('userName').value;
-    let password = document.getElementById('password').value;
-    let currentUser = users.find(function(user){
-        return user.name === username;
-    })
-    if (username === currentUser.name &&
-        password === currentUser.password &&
-        event.keyCode === 13) {
-            window.location = 'yeup.html'
-    } else if (
-        username !== currentUser.name || 
-        password !== currentUser.password &&
-        event.keyCode === 13) {
-            window.location = 'nope.html'
-        }
+    if (event.keyCode === 13) {
+        login();
+    }
 }
 
-// document.getElementById('login').addEventListener('click', clickLogin);
+function login() {
+    let currentUser = users.find(function(user){
+        return user.name === username.value;
+    })
+    if (username.value === currentUser.name &&
+        password.value === currentUser.password) {
+            window.location = 'yeup.html'
+    } else {
+        window.location = 'nope.html'
+    }
+}
+
+document.getElementById('login').addEventListener('click', login);
 document.getElementById('password').addEventListener('keydown', enterKeyLogin);
-
-
